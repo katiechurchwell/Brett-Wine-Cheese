50bf06437312cb9071c18b154dcbcb763885151e
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-25 14:16:41 -0700

    Contact bootstrap

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -35,27 +35,31 @@ function ContactForm() {
 
     return (
         <section>
-            <h1 data-testid="h1">Contact me</h1>
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+            <h1 data-testid="h1" className="m-3">Contact me</h1>
+            <div className="row d-flex justify-content-center m-3">
+                <div className="col-sm-4">
+                    <form id="contact-form" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">Name</label>
+                            <input type="text" className="form-control" defaultValue={name} onBlur={handleChange} name="name" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email">Email address</label>
+                            <input type="email" className="form-control" defaultValue={email} onBlur={handleChange} name="email" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="message" className="form-label">Message</label>
+                            <textarea rows="5" className="form-control" defaultValue={message} onBlur={handleChange} name="message" />
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
                 </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea rows="5" defaultValue={message} onBlur={handleChange} name="message" />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
-                    </div>
-                )}
-                <button type="submit">Submit</button>
-            </form>
+            </div>
         </section>
     )
 }

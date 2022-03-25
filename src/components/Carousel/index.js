a77558a9861e7ce02d046f1374b7f3d17c973e3a
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-24 17:25:23 -0700

    carousel images

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import image1 from '../../assets/images/baking/Photo6.jpg'
+import image2 from '../../assets/images/baking/Photo25.jpg'
+import image3 from '../../assets/images/baking/Photo18.jpg'
 
-function Carousel(props) {
-    
-return (
-    <p>
-        Carousel here!
-    </p>
-)
+function Carousel() {
+    return (
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src={image1} class="d-block w-100 h-80" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src={image2} class="d-block w-100 h-80" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src={image3} class="d-block w-100 h-80" alt="..." />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Carousel;

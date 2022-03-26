3bb1af15b799ea09fc0b7dafbbffcf009ec42bbd
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-26 16:52:40 -0700

    form tinkering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,54 +1,42 @@
 import React, { useState } from 'react';
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const { name, email, message } = formState;
-
-    const FORM_ENDPOINT = "mailto:churchwellcatherine@gmail.com";
-
-    const [submitted, setSubmitted] = useState(false);
-    const handleSubmit = () => {
-        setTimeout(() => {
-            setSubmitted(true);
-        }, 100);
-    };
-
-    if (submitted) {
-        return (
-            <>
-                <div className="text-2xl">Thank you!</div>
-                <div className="text-md">We'll be in touch soon.</div>
-            </>
-        );
-    }
+    const [formState, setFormState] = useState({ message: '' });
+    const { message } = formState;
+    const destination = "churchwell.catherine@gmail.com"
+    const link = `mailto:${destination}&subject=Hello%20Brett&body=${message}`
 
     return (
-        <form
-            action={FORM_ENDPOINT}
-            onSubmit={handleSubmit}
-            method="get"
-            enctype="text/plain">
-            <h1 data-testid="h1" className="m-3">Contact me</h1>
+        <section>
             <div className="row d-flex justify-content-center m-3">
+                <h1>Contact</h1>
                 <div className="col-sm-4">
-                    <form id="contact-form" onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="name" className="form-label">Name</label>
-                            <input type="text" className="form-control" defaultValue={name} name="name" />
+                    <form action="mailto:info@w3docs.com" method="get" enctype="text/plain">
+                        <div>
+                            <label className="form-label" for="name">Name</label>
+                            <br />
+                            <input type="text" className="form-control" name="name" id="name" />
                         </div>
-                        <div className="mb-3">
-                            <label htmlFor="email">Email address</label>
-                            <input type="email" className="form-control" defaultValue={email} name="email" />
+                        <div>
+                            <label className="form-label" for="email">Email</label>
+                            <br />
+                            <input type="text" className="form-control" name="email" id="email" />
                         </div>
-                        <div className="mb-3">
-                            <label htmlFor="message" className="form-label">Message</label>
-                            <textarea rows="5" className="form-control" defaultValue={message} name="message" />
+                        <div>
+                            <label className="form-label">Comments</label>
+                            <br />
+                            <textarea name="comments" className="form-control" rows="5" cols="35"></textarea>
                         </div>
-                        <button type="submit" value="Send" className="btn btn-primary">Submit</button>
+                        <div>
+                            <div className="row justify-content-center mt-3">
+                                <input type="submit" className="btn btn-primary m-1 w-25" name="submit" value="Send" />
+                                <input type="reset" className="btn btn-primary m-1 w-25" name="reset" value="Clear Form" />
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
-        </form>
+        </section>
     )
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,21 +6,14 @@ function Nav(props) {
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Brett Claus</a>
+                    <li className="navbar-brand">Brett Claus</li>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" onClick={ () => props.handleView('default')} href="#">Home</a>
+                            <li className="nav-link" onClick={() => props.handleView('default')}>Home
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={ () => props.handleView('portfolio')}href="#">Portfolio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={ () => props.handleView('about')} href="#">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={ () => props.handleView('contact')} href="#">Contact</a>
-                            </li>
+                            <li className="nav-link" onClick={() => props.handleView('portfolio')}>Portfolio</li>
+                            <li className="nav-link" onClick={() => props.handleView('about')}>About</li>
+                            <li className="nav-link" onClick={() => props.handleView('contact')}>Contact</li>
                         </ul>
                     </div>
                 </div>

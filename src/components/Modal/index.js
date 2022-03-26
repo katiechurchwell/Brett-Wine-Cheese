2e9e0de5899533b136135004c4736f62f6dff18c
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-26 15:23:03 -0700

    modal close and position

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,23 +5,17 @@ const Modal = ({ toggleModal, currentPhoto }) => {
     const i = currentPhoto + 1;
 
     return (
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <img 
-                        src={require(`../../images/original/Photo${i}.jpg`)} 
-                        alt="..." 
+        <div className="modal-dialog" onClick={() => toggleModal()}>
+            <div className="modal-content">
+                <div className="modal-body">
+                    <img
+                        src={require(`../../images/original/Photo${i}.jpg`)}
+                        alt="..."
                         className="w-100"
-                        />
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+                    />
                 </div>
             </div>
+        </div>
     );
 }
 

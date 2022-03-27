c58470ffbecd3760531efc7cd2fc65de245893ce
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-26 17:48:58 -0700

    stopping point: contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ message: '' });
-    const { message } = formState;
     const destination = "churchwell.catherine@gmail.com"
-    const link = `mailto:${destination}&subject=Hello%20Brett&body=${message}`
+    const link = `mailto:${destination}`
 
     return (
         <section>
             <div className="row d-flex justify-content-center m-3">
                 <h1>Contact</h1>
                 <div className="col-sm-4">
-                    <form action="mailto:info@w3docs.com" method="get" enctype="text/plain">
+                    <form>
                         <div>
                             <label className="form-label" for="name">Name</label>
                             <br />
                             <input type="text" className="form-control" name="name" id="name" />
-                        </div>
-                        <div>
-                            <label className="form-label" for="email">Email</label>
-                            <br />
-                            <input type="text" className="form-control" name="email" id="email" />
                         </div>
                         <div>
                             <label className="form-label">Comments</label>
@@ -29,7 +22,7 @@ function ContactForm() {
                         </div>
                         <div>
                             <div className="row justify-content-center mt-3">
-                                <input type="submit" className="btn btn-primary m-1 w-25" name="submit" value="Send" />
+                                <a href={link}><input type="submit" className="btn btn-primary m-1 w-25" name="submit" value="Send" /></a>
                                 <input type="reset" className="btn btn-primary m-1 w-25" name="reset" value="Clear Form" />
                             </div>
                         </div>

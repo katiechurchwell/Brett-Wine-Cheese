68f4da5751b0d620e18b585c6971b0a558a95256
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-26 17:37:40 -0700

    react-spring

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../Modal';
+import { useSpring, animated, config } from 'react-spring';
 
 function Portfolio() {
     const [currentPhoto, setCurrentPhoto] = useState();
@@ -14,7 +15,7 @@ function Portfolio() {
 
     return (
         <section>
-            {isModalOpen && <Modal toggleModal={toggleModal} currentPhoto={currentPhoto}/>}
+            {isModalOpen && <Modal toggleModal={toggleModal} currentPhoto={currentPhoto} />}
             <div className="row d-flex justify-content-center m-3">
                 <h1>Portfolio</h1>
                 <div className="album py-5 bg-light">
@@ -22,16 +23,18 @@ function Portfolio() {
                         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                             {[...Array(n)].map((image, i) =>
                                 <div key={i} className="col">
-                                    <div key={i} className="card shadow-sm">
-                                        <img
-                                            src={require(`../../images/thumbnail/Photo${i + 1}.jpg`)}
-                                            className="portfolio-img bd-placeholder-img card-img-top"
-                                            width="100%"
-                                            height="225"
-                                            key={i}
-                                            onClick={() => {toggleModal(); setCurrentPhoto(i)}}
-                                        />
-                                    </div>
+                                    <Card>
+                                        <div key={i} className="card shadow-sm">
+                                            <img
+                                                src={require(`../../images/thumbnail/Photo${i + 1}.jpg`)}
+                                                className="portfolio-img bd-placeholder-img card-img-top"
+                                                width="100%"
+                                                height="225"
+                                                key={i}
+                                                onClick={() => { toggleModal(); setCurrentPhoto(i) }}
+                                            />
+                                        </div>
+                                    </Card>
                                 </div>
                             )}
                         </div>
@@ -40,6 +43,24 @@ function Portfolio() {
             </div>
         </section>
     )
+
+    function Card({ children }) {
+        const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1]
+        const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+        const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: config.default }))
+
+        return (
+            <animated.div
+                onMouseMove={({ clientX: x, clientY: y }) => (set({ xys: calc(x, y) }))}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                style={{
+                    transform: props.xys.interpolate(trans)
+                }}
+            >
+                {children}
+            </animated.div>
+        );
+    }
 }
 
 export default Portfolio;
